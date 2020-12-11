1c5f41158983cb0618df0c7d9707752c5b6fb40e
Alexander Gherardi <alexander.2003@live.com>
2020-12-11 15:24:44 -0500

    Init Commit

---
+++ Tools/clean.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+nrfjprog --eraseall --snr $1
+nrfjprog --eraseall --snr $2
+export CONTROLLER_SN=$1
+export OUTLET_SN=$2
+platformio run -t upload -e nrf52840_dk -d ~/Documents/Projects/MeshController
+platformio run -t upload -e nrf52_dk -d ~/Documents/Projects/OutletFirmware
+
+# export CONTROLLER_SN='683897087'
+# export OUTLET_SN='682507407'
---
+++ Tools/setup.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+snap install android-studio --classic
+snap install flutter --classic
+snap install go --classic
+snap install code --classic
